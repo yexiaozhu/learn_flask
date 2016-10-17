9fa75aec2823c3d3a7c046c59c3d1bc8d2186539
yexiaozhu <557703988@139.com>
2016-10-17 03:55:44 -0700

    Chapter 2

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print"test 1a test tag"
+#!/usr/bin/env python2.7.11
+# -*- coding=utf8 -*-
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return '<h1>Hello World!</h1>'
+
+if __name__ == '__main__':
+	app.run(debug=True)
