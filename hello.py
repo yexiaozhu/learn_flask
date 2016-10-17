308dc7033a9a66b0b2fa6ad4dc402cd32a3ea3d2
yexiaozhu <557703988@139.com>
2016-10-17 05:01:11 -0700

    Chapter 02

--- hello.py
+++ hello.py
@@ -2,7 +2,11 @@
 # -*- coding=utf8 -*-
 
 from flask import Flask
+from flask_script import Manager
+
 app = Flask(__name__)
+
+manager = Manager(app)
 
 @app.route('/')
 def index():
@@ -13,4 +17,4 @@ def user(name):
 	return '<h1>Hello, %s!</h1>' %name
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	manager.run()
