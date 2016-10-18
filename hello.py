1ae95e3d02fefdb42a0cfae7387c3099da93dd05
yexiaozhu <557703988@139.com>
2016-10-18 02:46:10 -0700

    Chapter 03

--- hello.py
+++ hello.py
@@ -3,10 +3,12 @@
 
 from flask import Flask, render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -15,6 +17,14 @@ def index():
 @app.route('/user/<name>')
 def user(name):
 	return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+	return render_template('500.html'), 500
 
 if __name__ == '__main__':
 	manager.run()
