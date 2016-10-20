c4d765bfa3e38c00050b2bdf072083cf88b02646
yexiaozhu <557703988@139.com>
2016-10-20 01:37:40 -0700

    Chapter 04 04a

--- hello.py
+++ hello.py
@@ -6,20 +6,29 @@ from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(Form):
+	name = StringField('What is your name?', validators=[DataRequired()])
+	submit = SubmitField('Submit')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 moment = Moment(app)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html', current_time=datetime.utcnow())
-
-@app.route('/user/<name>')
-def user(name):
-	return render_template('user.html', name=name)
+	name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	return render_template('index.html', form=form, name=name)
 
 @app.errorhandler(404)
 def page_not_found(e):
