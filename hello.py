d138879d33556e3cb8fe048c3c15681f40f800ba
yexiaozhu <557703988@139.com>
2016-10-20 20:23:44 -0700

    Chapter 05:Database models with Flask-SQLAlchemy 05a

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.5
 # -*- coding=utf8 -*-
-
+import os
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
@@ -9,17 +9,44 @@ from datetime import datetime
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask.ext.sqlalchemy import SQLAlchemy
 
-class NameForm(Form):
-	name = StringField('What is your name?', validators=[DataRequired()])
-	submit = SubmitField('Submit')
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SECRET_KEY'] = 'hard to guess string'
 
 moment = Moment(app)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+
+class Role(db.Model):
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), unique=True)
+	users = db.relationship('User', backref='role')
+
+	def __repr__(self):
+		return '<Role %r>' % self.name
+
+class User(db.Model):
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), unique=True, index=True)
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+	def __repr__(self):
+		return '<User %r>' % self.username
+
+class NameForm(Form):
+	name = StringField('What is your name?', validators=[DataRequired()])
+	submit = SubmitField('Submit')
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
