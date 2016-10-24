c91b950954f42c2747d4c659cfb133ee37446dc0
yexiaozhu <557703988@139.com>
2016-10-23 23:51:59 -0700

    Chapter 06: Email support with Flask-Mail 06a

--- hello.py
+++ hello.py
@@ -11,6 +11,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Mail, Message
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -18,13 +19,21 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-
+app.config['MAIL_SERVER'] = 'smtp.139.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('xxxxxxx') #139邮箱账户
+app.config['MAIL_PASSWORD'] = os.environ.get('xxxxxx') #邮箱密码
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'xxxxxx'#发送邮箱
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 moment = Moment(app)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 manager.add_command('db', MigrateCommand)
 
 class Role(db.Model):
@@ -45,6 +54,13 @@ class User(db.Model):
 	def __repr__(self):
 		return '<User %r>' % self.username
 
+def send_email(to, subject, template, **kwargs):
+	msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
+				  sender=app.config['FLASKY_MAIL_SENDER'], recipients=['xxxxxx'])#接受邮箱
+	msg.body = render_template(template + '.txt', **kwargs)
+	msg.html = render_template(template + '.html', **kwargs)
+	mail.send(msg)
+
 class NameForm(Form):
 	name = StringField('What is your name?', validators=[DataRequired()])
 	submit = SubmitField('Submit')
@@ -62,11 +78,15 @@ def index():
 			user = User(username=form.name.data)
 			db.session.add(user)
 			session['known'] = False
+			if app.config['FLASKY_ADMIN']:
+				send_email(app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
 		else:
 			session['known'] = True
 		session['name'] = form.name.data
+		form.name.data = ''
 		return redirect(url_for('index'))
-	return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
+	return render_template('index.html', form=form, name=session.get('name'),
+						   known=session.get('known', False))
 
 @app.errorhandler(404)
 def page_not_found(e):
