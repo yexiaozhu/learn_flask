aba89ef46de47ad881d21c43dbb5e058751fa4f9
yexiaozhu <557703988@139.com>
2016-11-02 02:47:53 -0700

    Chapter 08: Login and logout with Flask-Login 08c

---
+++ app/auth/forms.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#coding=utf-8
+#author="yexiaozhu"
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log In')
