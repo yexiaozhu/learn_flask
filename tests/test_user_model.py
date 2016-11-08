d4198c1b1db3b830aa881d4e5dbf536962b1f16b
yexiaozhu <557703988@139.com>
2016-11-07 20:01:10 -0800

    Chapter 08: Account confirmation 08e

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -3,9 +3,22 @@
 #author="yexiaozhu"
 
 import unittest
+import time
+from app import create_app, db
 from app.models import User
 
 class UserModelTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
     def test_password_setter(self):
         u = User(password = 'yezi')
         self.assertTrue(u.password_hash is not None)
@@ -24,3 +37,27 @@ class UserModelTestCase(unittest.TestCase):
         u = User(password='yezi')
         u2 = User(password='yezi')
         self.assertTrue(u.password_hash != u2.password_hash)
+
+    def test_valid_confirmation_token(self):
+        u = User(password='yezi')
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_confirmation_token()
+        self.assertTrue(u.confirm(token))
+
+    def test_invalid_confirmation_token(self):
+        u1 = User(password='yezi')
+        u2 = User(password='yezi1')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        token = u1.generate_confirmation_token()
+        self.assertTrue(u2.confirm(token))
+
+    def test_expired_confirmation_token(self):
+        u = User(password='yezi')
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_confirmation_token()
+        time.sleep(2)
+        self.assertTrue(u.confirm(token))
