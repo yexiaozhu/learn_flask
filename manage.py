557569e2dd05d09354cf8378c3b40961fd2a2e84
yexiaozhu <557703988@139.com>
2016-11-17 02:38:58 -0800

    Chapter11 : Generate fake users and posts 11c

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
 from app import db, create_app
-from app.models import Role, User, Permission
+from app.models import Role, User, Permission, Post
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
@@ -15,7 +15,7 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role, Permission=Permission)
+    return dict(app=app, db=db, User=User, Role=Role, Permission=Permission, Post=Post)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
