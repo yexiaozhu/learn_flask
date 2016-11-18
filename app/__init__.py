d1ba60b7b1f54c3c367e630769861772453d01bf
yexiaozhu <557703988@139.com>
2016-11-17 21:44:29 -0800

    Chapter 11: Rich text blog posts with Flask-PageDown 11e

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@
 #author="yexiaozhu"
 
 from flask import Flask
+from flask_pagedown import PageDown
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
@@ -14,6 +15,7 @@ bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+pagedown = PageDown()
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -30,6 +32,7 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    pagedown.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 #coding=utf-8
 #author="yexiaozhu"
+from flask_pagedown.fields import PageDownField
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField
 from wtforms.validators import DataRequired, Length, Email, Regexp, ValidationError
@@ -48,5 +49,5 @@ class EditProfileAdminForm(FlaskForm):
             raise ValidationError('Username already in use.')
 
 class PostForm(FlaskForm):
-    body = TextAreaField("What's on your mind?", validators=[DataRequired()])
+    body = PageDownField("What's on your mind?", validators=[DataRequired()])
     submit = SubmitField('Submit')
